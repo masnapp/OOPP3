48d0610e38da771883085e4bbd2039364c96c9f2
Michael Snapp <masnapp2224@gmail.com>
2015-05-08 17:56:28 -0700

    Project 3

---
+++ OOPP3/Employee.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPP3
+{
+    class Employee
+    {
+        private int empDepNum;
+
+
+        private decimal hourlyPay;
+        private decimal hoursWorked;
+        private string[] lineInput;
+        Department[] departments;
+
+        public void setDepartments()
+        {
+            departments = new Department[7];
+            for (int i = 0; i < 6; i++)
+            {
+                departments[i] = new Department();
+            }
+        }
+
+        public void setLineInput(string[] input)
+        {
+            if (input.Length == 4)
+            {
+                lineInput = input;
+                this.validateEmployeeID();
+            }
+            else
+            {
+                Console.WriteLine("Error, the date line for this employee is not the correct length");
+            }
+        }
+
+        public void validateEmployeeID()
+        {
+            if (lineInput[0] != null)
+            {
+                setDepartmentNumber();
+            }
+        }
+
+        public void setDepartmentNumber()
+        {
+            if (int.TryParse(lineInput[1], out empDepNum) && empDepNum > 0 && empDepNum < 8)
+            {
+                setHourlyPay();
+            }
+            else
+            {
+                Console.WriteLine("Erorr with employees department number");
+            }
+        }
+
+        public void setHourlyPay()
+        {
+            if (decimal.TryParse(lineInput[2], out hourlyPay) && hourlyPay >= 10.0m)
+            {
+                setHoursWorked();
+            }
+            else
+            {
+                Console.WriteLine("Error witht the employees hourly pay");
+            }
+        }
+
+        public void setHoursWorked()
+        {
+            if (decimal.TryParse(lineInput[3], out hoursWorked) && hoursWorked > 0.00m)
+            {
+                setDepartmentPay();
+            }
+        }
+
+        public decimal calculatePay()
+        {
+            decimal pay = hourlyPay*hoursWorked;
+            return pay;
+        }
+
+        public void setDepartmentPay()
+        {
+            departments[empDepNum - 1].setDepartmentPay(calculatePay());
+        }
+
+    }
+}
---
+++ OOPP3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            
+            FileStream file = new FileStream("payroll.txt", FileMode.Open, FileAccess.Read);
+            StreamReader payroll = new StreamReader(file);
+
+            string line = payroll.ReadLine();
+             
+            emp.setDepartments();
+            while (line != null)
+            {
+                string[] fields = line.Split(',');
+
+                emp.setLineInput(fields);
+
+                line = payroll.ReadLine();
+            }
+            
+        }
+    }
+}
